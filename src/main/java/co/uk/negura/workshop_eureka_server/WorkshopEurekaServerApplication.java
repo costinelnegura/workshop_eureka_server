0cfb105d88b012f46b9bdb755911750e84e42c64
costinelnegura <pigeons-conduit0n@icloud.com>
2024-03-18 21:25:45 +0000

    initial commit

--- src/main/java/co/uk/negura/workshop_eureka_server/WorkshopEurekaServerApplication.java
+++ src/main/java/co/uk/negura/workshop_eureka_server/WorkshopEurekaServerApplication.java
@@ -2,8 +2,10 @@ package co.uk.negura.workshop_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class WorkshopEurekaServerApplication {
 
     public static void main(String[] args) {
